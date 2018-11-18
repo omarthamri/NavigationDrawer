99214ec57d566ca1025f6e40d6ce22d2388b7429
MACBOOK PRO RETINA <macbookproretina@MacBook-Pro-de-MACBOOK-2.local>
2018-11-18 11:06:06 +0100

    first commit

---
+++ NavigationDrawerDemo/HeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderTableViewCell.swift
+//  NavigationDrawerDemo
+//
+//  Created by MACBOOK PRO RETINA on 17/11/2018.
+//  Copyright © 2018 MACBOOK PRO RETINA. All rights reserved.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ NavigationDrawerDemo/MenuTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuTableViewCell.swift
+//  NavigationDrawerDemo
+//
+//  Created by MACBOOK PRO RETINA on 18/11/2018.
+//  Copyright © 2018 MACBOOK PRO RETINA. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
